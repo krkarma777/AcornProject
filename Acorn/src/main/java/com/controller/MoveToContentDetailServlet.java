121ee95f144256eebafc7fb8977161981c051431
ggiri <ggiri@ThinkpadSecond>
2024-01-22 18:17:10 +0900

    ayato 쓸데 없는 코드 지움 ㅎ.ㅎ

--- Acorn/src/main/java/com/controller/MoveToContentDetailServlet.java
+++ Acorn/src/main/java/com/controller/MoveToContentDetailServlet.java
@@ -9,34 +9,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Content
- */
 @WebServlet("/MoveToContentDetailServlet")
 public class MoveToContentDetailServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MoveToContentDetailServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		RequestDispatcher dis = request.getRequestDispatcher("WEB-INF/ContentDetail.jsp");//ContentDetail.jsp로 이동
 		dis.forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
--- Acorn/src/main/java/com/controller/MoveToContentsHomeServlet.java
+++ Acorn/src/main/java/com/controller/MoveToContentsHomeServlet.java
@@ -9,34 +9,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ContentsHome
- */
 @WebServlet("/MoveToContentsHomeServlet")
 public class MoveToContentsHomeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MoveToContentsHomeServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		RequestDispatcher dis = request.getRequestDispatcher("WEB-INF/ContentsHome.jsp");//ContentsHome.jsp로 이동
 		dis.forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
