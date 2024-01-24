c556c23c9f7a1031137575229d3e3b7318621ec0
krkarma777 <krkarma777@gmail.com>
2024-01-24 17:15:19 +0900

    feat : 게시글 출력을 위한 nickname 추가

--- Acorn/src/main/java/com/dto/PostDTO.java
+++ Acorn/src/main/java/com/dto/PostDTO.java
@@ -1,6 +1,5 @@
 package com.dto;
 
-import java.sql.Timestamp;
 import java.util.Date;
 
 public class PostDTO {
@@ -12,89 +11,86 @@ public class PostDTO {
     private Date postDate;
     private Date postEditDate;
     private String postText;
-
-    // 생성자
-    public PostDTO() {
-    }
-    
-    // 사용자 지정 생성자
-    public PostDTO(String userId, String postTitle, String postText, String postBoard) {
-        this.userId = userId;
-        this.postTitle = postTitle;
-        this.postText = postText;
-        this.postBoard = postBoard;
-    }
-
-    @Override
+    private String nickname;
+	public PostDTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public PostDTO(Long postId, String postBoard, String userId, Long contId, String postTitle, Date postDate,
+			Date postEditDate, String postText, String nickname) {
+		super();
+		this.postId = postId;
+		this.postBoard = postBoard;
+		this.userId = userId;
+		this.contId = contId;
+		this.postTitle = postTitle;
+		this.postDate = postDate;
+		this.postEditDate = postEditDate;
+		this.postText = postText;
+		this.nickname = nickname;
+	}
+	public Long getPostId() {
+		return postId;
+	}
+	public void setPostId(Long postId) {
+		this.postId = postId;
+	}
+	public String getPostBoard() {
+		return postBoard;
+	}
+	public void setPostBoard(String postBoard) {
+		this.postBoard = postBoard;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public Long getContId() {
+		return contId;
+	}
+	public void setContId(Long contId) {
+		this.contId = contId;
+	}
+	public String getPostTitle() {
+		return postTitle;
+	}
+	public void setPostTitle(String postTitle) {
+		this.postTitle = postTitle;
+	}
+	public Date getPostDate() {
+		return postDate;
+	}
+	public void setPostDate(Date postDate) {
+		this.postDate = postDate;
+	}
+	public Date getPostEditDate() {
+		return postEditDate;
+	}
+	public void setPostEditDate(Date postEditDate) {
+		this.postEditDate = postEditDate;
+	}
+	public String getPostText() {
+		return postText;
+	}
+	public void setPostText(String postText) {
+		this.postText = postText;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	@Override
 	public String toString() {
 		return "PostDTO [postId=" + postId + ", postBoard=" + postBoard + ", userId=" + userId + ", contId=" + contId
 				+ ", postTitle=" + postTitle + ", postDate=" + postDate + ", postEditDate=" + postEditDate
-				+ ", postText=" + postText + "]";
+				+ ", postText=" + postText + ", nickname=" + nickname + "]";
 	}
+	
 
-	// 게터 및 세터
-    public Long getPostId() {
-        return postId;
-    }
-
-    public void setPostId(Long postId) {
-        this.postId = postId;
-    }
-
-    public String getPostBoard() {
-        return postBoard;
-    }
-
-    public void setPostBoard(String postBoard) {
-        this.postBoard = postBoard;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public Long getContId() {
-        return contId;
-    }
-
-    public void setContId(Long contId) {
-        this.contId = contId;
-    }
-
-    public String getPostTitle() {
-        return postTitle;
-    }
-
-    public void setPostTitle(String postTitle) {
-        this.postTitle = postTitle;
-    }
-
-    public Date getPostDate() {
-        return postDate;
-    }
-
-    public void setPostDate(Date postDate) {
-        this.postDate = postDate;
-    }
-
-    public Date getPostEditDate() {
-        return postEditDate;
-    }
-
-    public void setPostEditDate(Date postEditDate) {
-        this.postEditDate = postEditDate;
-    }
-
-    public String getPostText() {
-        return postText;
-    }
-
-    public void setPostText(String postText) {
-        this.postText = postText;
-    }
+    
 }
 
