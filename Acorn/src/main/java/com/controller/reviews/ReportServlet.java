83a5f3656522d254b9ce4990b6f434916e541a8c
bsj4387 <7834jsb@gmail.com>
2024-02-19 17:18:51 +0900

    feat.
    리뷰 자세히보기 화면 개선
    리뷰 자세히보기 화면 댓글기능 연동
    리뷰 신고 기능 추가
    ReportDTO 추가
    자잘한 기능 수정

--- Acorn/src/main/java/com/controller/reviews/ReportServlet.java
+++ Acorn/src/main/java/com/controller/reviews/ReportServlet.java
@@ -20,7 +20,7 @@ import com.service.ReviewService;
 public class ReportServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    public ReportServlet() {
+    public ReportServlet(){
         super();
     }
 
